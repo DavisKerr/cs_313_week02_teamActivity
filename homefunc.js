a50b357c31a4e35893adefada5b15d05315c704e
Davis Kerr <davis.kerr1997@gmail.com>
2020-09-18 13:50:36 -0600

    Added a color picker

--- homefunc.js
+++ homefunc.js
@@ -2,3 +2,27 @@ function alertButton()
 {
   alert("Clicked!")
 }
+
+function changeColor()
+{
+  var color = document.getElementById('color').value;
+  errors = document.getElementById('errorMsg');
+  if(validateColor(color) == true)
+  {
+    var divider = document.getElementsByClassName('colorful')[0];
+    divider.style.backgroundColor = color;
+    errors.innerHTML = '';
+  }
+  else
+  {
+    errors.innerHTML = 'Not a valid hex color. Remember the format is #000000'
+  }
+  
+}
+
+function validateColor(color)
+{
+  const regex = RegExp('^#[a-f|A-F|0-9]{6}$');
+  var result = regex.test(color);
+  return result;
+}
