584880ca64771f4e0ddf3b85cda7c58c7e81c73d
Davis Kerr <davis.kerr1997@gmail.com>
2020-09-18 14:54:56 -0600

    Replaced the color picker code with JQuery

--- homefunc.js
+++ homefunc.js
@@ -2,16 +2,19 @@ function alertButton()
 {
   alert("Clicked!")
 }
-
+/*
 function changeColor()
 {
-  var color = document.getElementById('color').value;
+  var colorPicker = document.getElementById('color');
+  var color = colorPicker.value;
   errors = document.getElementById('errorMsg');
-  if(validateColor(color) == true)
+
+  if (validateColor(color) == true)
   {
     var divider = document.getElementsByClassName('colorful')[0];
     divider.style.backgroundColor = color;
     errors.innerHTML = '';
+    colorPicker.value = '';
   }
   else
   {
@@ -19,10 +22,30 @@ function changeColor()
   }
   
 }
-
+*/
 function validateColor(color)
 {
   const regex = RegExp('^#[a-f|A-F|0-9]{6}$');
   var result = regex.test(color);
   return result;
 }
+
+$(document).ready(function(){
+  
+
+  $("#colorChange").click(function(){
+
+    color = $("#color").val();
+    if (validateColor(color))
+    {
+      $(".colorful").first().css("background-color", color);
+      $("color").val('');
+    }
+    else
+    {
+      $("#errorMsg").text('Invalid hex code. Please us the format #000000');
+    }
+
+  });
+
+});
